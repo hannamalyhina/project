ed217e656cda650720471234c2464066df73602f
Anna <example@gmail.com>
2022-09-19 15:44:49 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use stright";
+// alert('Hello!!');
+// const result = confirm('Are your here?');
+// console.log(result);
+// const answer = +prompt('Тебе есть 18?');
+// console.log(typeof(answer));
+
+const answers = [];
+answers[0] = prompt('How old are you?', '');
+answers[1] = prompt('What is your name?', '');
+answers[2] = prompt('Where are you?', '');
+console.log(answers);
+
+document.write(answers);
+
+
+
