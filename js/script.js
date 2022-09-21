6367642d735dfdfc3c1eb3d36f9bca271d3cfe23
Anna <example@gmail.com>
2022-09-21 13:07:04 +0200

    the beginning of the first application

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
-"use stright";
-// alert('Hello!!');
-// const result = confirm('Are your here?');
-// console.log(result);
-// const answer = +prompt('Тебе есть 18?');
-// console.log(typeof(answer));
-
-const answers = [];
-answers[0] = prompt('How old are you?', '');
-answers[1] = prompt('What is your name?', '');
-answers[2] = prompt('Where are you?', '');
-console.log(answers);
-
-document.write(answers);
-
-
+'use strict';
+console.log(1);
+const number0film = +prompt("Сколько фильмов ты уже посмотрел?", "");
+console.log(number0film);
+const personalMovieDB = {
+    count: number0film,
+    movies: {},
+    actors: {},
+    gentres: [],
+    privat: false
+};
+const a = prompt("Один из последних просмотернных фильмов?", "");
+const b = prompt("На сколько его оцените?", "");
+const c = prompt("Один из последних просмотернных фильмов?", "");
+const d = prompt("На сколько его оцените?", "");
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
 
